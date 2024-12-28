124f532cba9547cf2dec0b98cfd555bef998dfc6
Izztl <133669363+ZHudxxx@users.noreply.github.com>
2024-12-29 02:07:18 +0800

    Update admin_about.php

--- admin_about.php
+++ admin_about.php
@@ -30,7 +30,7 @@ if (isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo htmlspecialchars($article['title']); ?> | Mindful Pathway</title>
+    <title> About | Mindful Pathway</title>
     <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
