550ad1a59101255acf49473f44e2d9e58533492f
Izztl <133669363+ZHudxxx@users.noreply.github.com>
2024-12-28 20:44:10 +0800

    Update user_home.php

--- user_home.php
+++ user_home.php
@@ -197,7 +197,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     }
 
     .article-card {
-      width: 400px;
+      width: 500px;
       background-color: white;
       border-radius: 8px;
       box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
