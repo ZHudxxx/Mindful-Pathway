8be525215df6c521903d7712a92888a384b26910
Izztl <133669363+ZHudxxx@users.noreply.github.com>
2025-01-01 21:06:23 +0800

    Update article_manage.php

--- article_manage.php
+++ article_manage.php
@@ -409,7 +409,7 @@ footer {
     <a href="admin_about.php">About</a>
     <a href="admin_profile.php">My Profile</a>
     <a href="article_manage.php" class="active">Manage Articles</a>
-    <a href="user_manage.php">Manage Users</a>
+    <a href="admin_user_manage.php">Manage Users</a>
     <a href="feedback.html">Feedback</a>
 
     <a href="logout.php" class="logout">Logout</a>
