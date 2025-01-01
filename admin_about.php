e7e9f98e2f20a34404453b5663e14fced1754c82
Izztl <133669363+ZHudxxx@users.noreply.github.com>
2025-01-01 21:04:26 +0800

    Update admin_about.php

--- admin_about.php
+++ admin_about.php
@@ -341,7 +341,7 @@ if (isset($_SESSION['username'])) {
   <a href="admin_about.php"  class="active">About</a>
   <a href="admin_profile.php">My Profile</a>
   <a href="article_manage.php">Manage Articles</a>
-  <a href="user_manage.php">Manage Users</a>
+  <a href="admin_user_manage.php">Manage Users</a>
   <a href="feedback.html">Feedback</a>
   <a href="logout.php" class="logout">Logout</a>
 </div>
