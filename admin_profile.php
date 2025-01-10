a42e1137df3129e9bdb38ad4bb3e5c65153ae7e7
Izztl <133669363+ZHudxxx@users.noreply.github.com>
2025-01-10 20:30:21 +0800

    Update admin_profile.php
    admin_feedback.php

--- admin_profile.php
+++ admin_profile.php
@@ -390,7 +390,7 @@ $admin = $result->fetch_assoc();
         <a href="admin_profile.php" class="active">My Profile</a>
         <a href="article_manage.php">Manage Articles</a>
         <a href="admin_user_manage.php">Manage Users</a>
-        <a href="feedback.html">Feedback</a>
+        <a href="admin_feedback.php">Feedback</a>
         <a href="logout.php" class="logout">Logout</a>
     </div>
 
