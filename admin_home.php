f5a1f1661d0e2b50f7239d42de8614b640c221ac
Izztl <133669363+ZHudxxx@users.noreply.github.com>
2025-01-10 20:29:02 +0800

    Update admin_home.php
    admin_feedback.php

--- admin_home.php
+++ admin_home.php
@@ -426,7 +426,7 @@ footer {
   <a href="admin_profile.php">My Profile</a>
   <a href="article_manage.php">Manage Articles</a>
   <a href="admin_user_manage.php">Manage Users</a>
-  <a href="feedback.html">Feedback</a>
+  <a href="admin_feedback.php">Feedback</a>
   <a href="logout.php" class="logout">Logout</a>
 </div>
 
